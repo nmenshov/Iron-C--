786d7a2d6b8dbfbc7bb612e7482e8e6af897d5b2
CMogilko <CMogilko@gmail.com>
2013-04-09 22:16:09 +0400

    Init project

---
+++ IronC--Compiler/IronC--Syntax/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronC__Syntax
+{
+    public class Class1
+    {
+    }
+}
